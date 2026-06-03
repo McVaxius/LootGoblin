f2aec69043549e2ffde48c86c2ea28178d97ef98
davoodinator <135515620+McVaxius@users.noreply.github.com>
2026-06-02 22:03:31 -0400

    Successfully bumped LootGoblin to version 0.3.5.1

--- LootGoblin/Services/RetainerMapRetrievalService.cs
+++ LootGoblin/Services/RetainerMapRetrievalService.cs
@@ -1288,7 +1288,7 @@ public sealed class RetainerMapRetrievalService : IDisposable
             if (value->Type == 0)
                 return string.Empty;
 
-            if (value->Type is not (AtkValueType.String or AtkValueType.ManagedString or AtkValueType.String8 or AtkValueType.WideString))
+            if (value->Type is not (AtkValueType.String or AtkValueType.ManagedString or AtkValueType.ConstString or AtkValueType.WideString))
                 return string.Empty;
 
             return value->String.Value == null
